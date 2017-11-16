83caf29e9e1c8c2de62ba31eb60141cc7230a6e4
candle <ted@wicrep.com>
2017-11-16 17:30:32 +0900

    Create main program

---
+++ src/index.js
@@ -0,0 +1,35 @@
+module.exports = babel => {
+  const t = babel.types;
+  return {
+    name: "s2s-axios-apis",
+    visitor: {
+      ExpressionStatement: function(path){
+        const actionName = path.node.expression.name
+        if(actionName == undefined){
+          return
+        }
+        path.replaceWith(
+          t.ExportNamedDeclaration(
+            t.VariableDeclaration(
+              "const",
+              [t.VariableDeclarator(
+                t.Identifier(actionName),
+                t.ArrowFunctionExpression(
+                  [t.Identifier("config")],
+                  t.CallExpression(
+                    t.MemberExpression(t.Identifier("axios"),t.Identifier("get")),
+                    [t.TemplateLiteral(
+                      [t.TemplateElement({raw: ""})],[]
+                    ),
+                     t.Identifier("config")
+                    ]
+                  )
+                )
+              )]
+            ),[]
+          )
+        )
+      }
+    }
+  };
+}
