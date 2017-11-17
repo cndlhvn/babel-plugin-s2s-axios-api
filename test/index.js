5d0db5a907fd71bc388ef40ad1921f1a56845f16
candle <ted@wicrep.com>
2017-11-17 22:56:49 +0900

    Change test files

--- test/index.js
+++ test/index.js
@@ -1,9 +1,2 @@
-import axios from 'axios'
-
-const API_ROOT = process.env.API_ROOT || 'http://localhost:3000'
-
-axios.defaults.timeout = 5000
-axios.defaults.baseURL = API_ROOT
-axios.defaults.headers.post['Content-Type'] = 'application/json'
-
+import axios from "./setting"
 getPokemonRequest
